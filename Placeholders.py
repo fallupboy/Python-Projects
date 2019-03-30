b8e19db140cd5796fc2c055fdbf7a8a43e7e3353
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-03-30 20:45:58 +0200

    Add files via upload

---
+++ Placeholders.py
@@ -0,0 +1,5 @@
+varName = input("Enter Your name: ")
+varSurname = input("Enter Your surname: ")
+fullname = varName + " " + varSurname
+greeting = ("Hello, %s, have a nice day :)")
+print(greeting % fullname)

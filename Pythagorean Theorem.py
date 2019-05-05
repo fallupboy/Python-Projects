869fd56f0679f774c1303961913535fcb625ba9a
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-05-05 19:12:10 +0300

    Add files via upload

---
+++ Pythagorean Theorem.py
@@ -0,0 +1,17 @@
+import math
+print("1.The first catheter")
+print("2.The second catheter")
+print("3.Hypotenuse")
+toDo = int(input("Choose a number what you want to find: "))
+if toDo == 1:
+    secondCatheter = int(input("Set the value to the second catheter: "))
+    hypotenuse = int(input("Set the value of the hypotenuse: "))
+    print("The first catheter equales", math.sqrt(hypotenuse ** 2 - secondCatheter ** 2))
+elif toDo == 2:
+    firstCatheter = int(input("Set the value to the first catheret: "))
+    hypotenuse = int(input("Set the value of the hypotenuse: "))
+    print("The second catheter equales", math.sqrt(hypotenuse ** 2 - firstCatheter ** 2))
+else:
+    firstCatheter = int(input("Set the value to the first catheret: "))
+    secondCatheter = int(input("Set the value to the second catheter: "))
+    print("Hypotenuse equales", math.sqrt(firstCatheter ** 2 + secondCatheter ** 2))

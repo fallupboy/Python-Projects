2f92283f63e4635f3ad7a4e653eeeacbe776aeca
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-05-06 14:38:53 +0300

    Update Game Of Thrones Serial Guide.py

--- Game Of Thrones Serial Guide.py
+++ Game Of Thrones Serial Guide.py
@@ -1,10 +1,10 @@
 import webbrowser
 
-link = 'http://hdrezka-ag.com/series/fantasy/45-igra-prestolov-2011-tv-series-1.html#t:65-s:season-e:episodes'
+link = 'http://hdrezka-ag.com/series/fantasy/45-igra-prestolov-2011-tv-series-1.html#t:65-s:season-e:episode'
 print("Hey! I can help you to navigate to the 'Game of Thrones' any season and episode!")
 season = (input("First type the number of the season you want to watch: "))
-episodes = (input("Now type the number of the episode you want to see: "))
-choice = [season, episodes]
+episode = (input("Now type the number of the episode you want to see: "))
+choice = [season, episode]
 if choice[0] > '8':
     print("Incorrect value. GoT has only 8 seasons! Try again.", )
     exit(0)
@@ -15,5 +15,5 @@ elif choice[0] == '8' and choice[1] > '4':
     print("Incorrect value. Season 8 has 6 episodes and there are only 4 series have been released. Try again!",)
     exit(0)
 print("Enjoy:)")
-readyLink = link.replace('season', season).replace('episodes', episodes)
+readyLink = link.replace('season', season).replace('episode', episode)
 webbrowser.open(readyLink)

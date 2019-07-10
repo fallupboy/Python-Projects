32c441076ff0dcb1e676e6a8fe2368ab1ba151c8
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-07-10 23:17:28 +0300

    Update school.py
    -Some functions were fixed

--- School Graph/school.py
+++ School Graph/school.py
@@ -1,5 +1,5 @@
 import json
-path = 'C:/Users/FallUpBoy/Desktop/Python projects/school graph/school.json'
+path = 'C:/Users/FallUpBoy/Desktop/Python projects/school graph project/school.json'
 with open(path, 'r') as f:
 	data = json.loads(f.read())
 print("What do you want to check?")
@@ -28,6 +28,7 @@ if operation == 1:
     for i in data['students']:
     	if i['id'] == getFavPerson(friendsList):
 		    print("The most popular student in class is:", i['name'])
+    input("Press 'ENTER' to exit")
 elif operation == 2:
     favTeacherList = []
     for i in data['students']:
@@ -35,6 +36,7 @@ elif operation == 2:
     for i in data['teachers']:
         if i['id'] == getFavPerson(favTeacherList):
             print("The most popular teacher in class is:", i['name'])
+    input("Press 'ENTER' to exit")
 elif operation == 3:
     studentsAmount = []
     teachersAmount = []
@@ -43,22 +45,37 @@ elif operation == 3:
     for i in data['teachers']:
         teachersAmount.append(i['id'])
     print("There are", len(studentsAmount + teachersAmount), "people in school.")
+    input("Press 'ENTER' to exit")
 elif operation == 4:
     studentsName = []
+    letterCheckM = []
     for i in data['students']:
         studentsName.append(i['name'])
-    print("Here are all students whoose First Name starts with 'M':")
     for i in studentsName:
         if i[0] == "M":
+            letterCheckM.append(i)
+    if len(letterCheckM) > 0:
+        print("Here are all students whoose First Name starts with 'M':")
+        for i in letterCheckM:
             print(i)
+    else:
+        print("There are no students whoose First Name starts with 'M'")
+    input("Press 'ENTER' to exit")  
 elif operation == 5:
     teachersSplit = []
+    letterCheckG = []
     for i in data['teachers']:
         teachersSplit.append(i['name'].split())
-    print("Here are all teachers whoose Second Name starts with 'G':")
     for i in teachersSplit:
         if i[1][0] == "G":
+            letterCheckG.append(i)
+    if len(letterCheckG) > 0:
+        print("Here are all teachers whoose Second Name starts with 'G':")
+        for i in letterCheckG:    
             print(i[0], i[1])
+    else:
+        print("There are no teachers whoose Last Name starts with 'G'")
+    input("Press 'ENTER' to exit")
 elif operation == 6:
     favStudentList = []
     for i in data['teachers']:
@@ -66,7 +83,7 @@ elif operation == 6:
     for i in data['students']:
         if i['id'] == getFavPerson(favStudentList):
             print("The favorite student among teachers is:", i['name'])
+    input("Press 'ENTER' to exit")
 else:
     print("Invalid input")
-    exit(0)
-	
+    input("Press 'ENTER' to exit")

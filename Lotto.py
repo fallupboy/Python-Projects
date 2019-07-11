5c814d7947e083437699cafe51b274ce5669920d
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-07-11 16:09:49 +0300

    Add files via upload
    v1.0
    My first lottery playing system made by Python

---
+++ Lotto.py
@@ -0,0 +1,31 @@
+from random import random
+
+loop = True
+attemps = 4
+jackpot = 1000
+
+print(20 * "-", "Welcome to Python Lotto! Today's jackpot is 1000$", 20 * "-")
+print(23 * "-", "You will have 5 attemps to win our jackpot", 23 * "-") 
+print(20 * "-", "Every unlucky attemp will raise jackpot by twice", 20 * "-")
+
+while loop:
+    winNum = random() * 19 + 1
+    winNum = int(winNum)
+    userNum = int(input("Enter a number to play (1 - 20): "))
+    if userNum < 1 or userNum > 20:
+        print("Invalid number. Choose between 1-20")
+        exit(0)
+    if userNum == winNum:
+        print("Congratulations! You won", jackpot, "$")
+        input("Press ENTER to exit")
+        exit(0)
+    else:
+        print("Unlucky attemp... The winning number was", winNum, ".", "Try again!") 
+        print(attemps, "attemps remained. Jackpot is raised to", jackpot * 2, "$")
+        print(50 * "-")
+        attemps -= 1
+        jackpot *= 2
+    if attemps < 0:
+        print("Sorry, but there are no attemps left."), 
+        input("Press ENTER to exit")
+        exit(0)

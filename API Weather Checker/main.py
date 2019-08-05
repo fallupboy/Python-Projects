46cbb5f252ded70d53df383763f43b9889166288
fallupboy <36111636+fallupboy@users.noreply.github.com>
2019-08-05 14:19:44 +0300

    Add files via upload

---
+++ API Weather Checker/main.py
@@ -0,0 +1,44 @@
+import requests
+import datetime
+import time
+
+humidity = []
+date = []
+temp = []
+desc = []
+
+errorCheck = True
+while errorCheck:
+    try:
+        city_name = input("Enter city name: ")
+        country_code = input("Enter country code (ISO): ")
+        app_id = '7b9d5008d599ec2605884c73a1a4bcb2'
+        complete_url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city_name + ',' + country_code + '&appid=' + app_id + '&mode=json&units=metric'
+        data = requests.post(complete_url)
+        data = data.json()
+
+        for lists in data['list']:
+            date.append(lists['dt_txt'])
+            humidity.append(lists['main']['humidity'])
+            temp.append(lists['main']['temp'])
+            desc.append(lists['weather'][0]['description'])
+
+        print("*" * 80)
+        print("\t\t" + "Details about the forecast of the city " + city_name + " for 5 days:\n")
+        print('{:^25}{:^20}{:^20}{:^20}'.format("Date", "Description", "Temperature", "Humidity\n"))
+        for i in range(len(humidity)):
+            print('{:^25}{:^20}{:^20}{:^20}'.format(str(date[i]), desc[i], str(temp[i]) + " C\N{DEGREE SIGN}", str(humidity[i]) + " %"))
+        errorCheck = False
+    except KeyError:
+            print("City not found. Try again.")
+            print()
+
+
+path = 'C:/Users/FallUpBoy/Desktop/Python projects/API Weather Checker/log.txt'
+with open(path, "a+") as file:
+    date = datetime.datetime.now().strftime("%m/%d/%y | %H:%M:%S")
+    file.write('Logged data at: ' + str(date) + '\n')
+print()
+print("*" * 80)
+input("Press ENTER to exit")
+exit(0)
